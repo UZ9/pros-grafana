bfa79de61ed37a311f31624c827709b521489750
Yerti <36551149+UZ9@users.noreply.github.com>
2021-07-01 18:45:35 -0500

    :hammer: Refactor the OdometryPanel + Misc Changes
    - Removed the panel options from the OdometryPanel, as they weren't exactly necessary.
    - Rename SimplePanel.tsx to OdometryPanel.tsx, and clean up a few old comments
    - Add a safeguard "No odometry data was found" text when the SQLite query fails

--- pros-grafana/src/OdometryPanel.tsx
+++ pros-grafana/src/OdometryPanel.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
-import { css, cx } from 'emotion';
-import { stylesFactory, ThemeContext, useTheme } from '@grafana/ui';
+import { css } from 'emotion';
+import { stylesFactory, useTheme } from '@grafana/ui';
 import { Marker } from "marker";
 
 interface Props extends PanelProps<SimpleOptions> { }
@@ -17,38 +17,56 @@ const dataS = [
 ];
 
 function translatePosToGui(x: number, y: number, currentWidth: number): [number, number] {
-  const scale = currentWidth / 6.0; 
+  const scale = currentWidth / 6.0;
 
   return [x * scale, y * scale];
 }
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+export const OdometryPanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
-  const styles = getStyles();
 
-  
+
   // Handle resizing the panel
   const size = width / 6;
 
 
 
-  const dataVals = data.series 
+  const dataVals = data.series
     .map(series => series.fields.find(field => field.type == "number" && field.name != "date"))
-  
+
   // I'm sure there's a better way of doing this, but I don't know enough of grafana to think of a good solution
   const dataRobotXField = dataVals.find(f => f?.name == "robotx");
   const dataRobotYField = dataVals.find(f => f?.name == "roboty");
   const dataRobotHeadingField = dataVals.find(f => f?.name == "robotheading");
 
-  const dataRobotX = dataRobotXField?.values.get(dataRobotXField.values.length - 1);
-  const dataRobotY = dataRobotYField?.values.get(dataRobotYField.values.length - 1);
-  const dataRobotHeading = dataRobotHeadingField?.values.get(dataRobotHeadingField.values.length - 1);
+  let dataRobotX = dataRobotXField?.values.get(dataRobotXField.values.length - 1);
+  let dataRobotY = dataRobotYField?.values.get(dataRobotYField.values.length - 1);
+  let dataRobotHeading = dataRobotHeadingField?.values.get(dataRobotHeadingField.values.length - 1);
+
+  // If the data is invalid or it can't find the given variables, set the default values
+  if (isNaN(dataRobotHeading)) {
+    return (
+      <div style={{ position: "relative", overflow: 'clip', width: width, height: height}}>
+        <div style={{ textAlign:"center", display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", width, height }}>
+          <ul>
+            <li>
+              <p>
+                No odometry data was found.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 
   // Heading calculations
   const radianHeading = (dataRobotHeading - 90) * Math.PI / 180.0;
 
   const xOffset = Math.cos(radianHeading) * (30 + width / 30);
   const yOffset = Math.sin(radianHeading) * (30 + width / 30);
+
+
 
   const [robotX, robotY] = translatePosToGui(dataRobotX, dataRobotY, width);
 
@@ -60,27 +78,13 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
 
         <table style={{ border: "1px solid #35393c", overflow: 'clip' }}>
           {
-            dataS.map((row, index) => (
+            dataS.map((row) => (
               <tr key={row[0]}>
                 {row.map(cellId => <th style={{ border: "1px solid #35393c", textAlign: 'center', width: size, height: size, padding: "5", backgroundColor: "#0f1015" }} key={cellId}></th>)}
               </tr>
             ))
           }
         </table>
-
-        {/* style={{
-        height: dotRadius * 2,
-        width: dotRadius * 2,
-        backgroundImage: `url(${marker})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        position: "absolute",
-        borderRadius: dotRadius,
-        // transform: `translate(${-dotRadius}, ${-dotRadius})`,
-        top: y - (dotRadius) - 2,
-        left: x - (dotRadius) - 2
-      }} */}
-
 
         <div style={{ position: "absolute", top: robotY - 200, left: robotX - 200 }}>
           <svg width={width} height={height}>
@@ -102,22 +106,3 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
 
   );
 };
-
-const getStyles = stylesFactory(() => {
-  return {
-    wrapper: css`
-      position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
-    `,
-  };
-});
